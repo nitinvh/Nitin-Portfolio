fabeca02742116f7c7e2c67feb175412898609cc
Nitin V H <nikki@Nitins-MacBook-Air-3.local>
2025-01-06 21:15:21 -0600

    Changed profile pic

--- src/components/MyProfilePic.jsx
+++ src/components/MyProfilePic.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Image } from 'react-bootstrap';
-import ProfilePic from '../../public/profile_pic.jpg';
+import ProfilePic from '../../src/assets/profilePic-.png';
 
 const MyProfilePic = () => {
     return (
         <div className="d-flex justify-content-center align-items-center">
-            <img src={ProfilePic} className="rounded-circle border border-3 profile-border-color p-3 profile-pic my-3" />
+            <img 
+                src={ProfilePic} 
+                className="rounded-circle border border-3 profile-border-color p-3 profile-pic my-3" 
+                alt="Profile" 
+            />
         </div>
     );
 }
--- src/components/eperience/Experience.jsx
+++ src/components/eperience/Experience.jsx
@@ -11,42 +11,36 @@ const Experience = () => {
 
   const navigate = useNavigate();
 
-  const handleOnClick = (id) => {
-    sessionStorage.setItem('id', id);
-    navigate('/experience-detail-view');
-
-  }
-
-  const renderTooltip = (props) => (
-    <Tooltip id="button-tooltip" className='fs-5 fw-bolder' {...props}>
-      Click me for detail view
-    </Tooltip>
-  );
+  // const renderTooltip = (props) => (
+  //   <Tooltip id="button-tooltip" className='fs-5 fw-bolder' {...props}>
+  //     Click me for detail view
+  //   </Tooltip>
+  // );
 
   return (
     <div className="mt-6 ">
       <div className="mx-5">
         <h2 className="fw-bolder text-start mb-5 custom-font-size pt-5 text-light" id='experience'>Experience</h2>
         {experiences.map((exp, index) => (
-          <div key={index} className={`card bg-dark border-bottom border-bottom border-0 rounded-0 py-5 custom-border-color`} onClick={() => handleOnClick(exp?.id)}>
+          <div key={index} className={`card bg-dark border-bottom border-bottom border-0 rounded-0 py-5 custom-border-color`} >
 
             <div className="row g-0 mb-5">
               {/* Left Side: Company Logo and Summary */}
-              <OverlayTrigger
+              {/* <OverlayTrigger
                 placement="bottom"
                 delay={{ show: 250, hide: 400 }}
                 overlay={renderTooltip}
-              >
-                <div className="col-lg-4 d-flex align-items-center justify-content-center p-3 flex-column point">
-                  <img src={exp.companyLogo} className="img-fluid rounded-circle border border-4  mb-3" alt={`${exp.company} logo`} style={{ width: '10rem', height: '10rem' }} />
-                  <div className="text-center text-light mt-3">
-                    <h5 className="card-title fs-2 text-theme-color">{exp.title}</h5>
-                    <p className="card-text fs-1 "><strong>{exp.company}</strong> · <span className="third-theme-clr">{exp.employmentType}</span></p>
-                    <p className="card-text fs-3 fourth-theme-color">{exp.duration}</p>
-                    <p className="card-text fs-3 fourth-theme-color">{exp.location}</p>
-                  </div>
+              > */}
+              <div className="col-lg-4 d-flex align-items-center justify-content-center p-3 flex-column ">
+                <img src={exp.companyLogo} className="img-fluid rounded-circle border border-4  mb-3" alt={`${exp.company} logo`} style={{ width: '10rem', height: '10rem' }} />
+                <div className="text-center text-light mt-3">
+                  <h5 className="card-title fs-2 text-theme-color">{exp.title}</h5>
+                  <p className="card-text fs-1 "><strong>{exp.company}</strong> · <span className="third-theme-clr">{exp.employmentType}</span></p>
+                  <p className="card-text fs-3 fourth-theme-color">{exp.duration}</p>
+                  <p className="card-text fs-3 fourth-theme-color">{exp.location}</p>
                 </div>
-              </OverlayTrigger>
+              </div>
+              {/* </OverlayTrigger> */}
               {/* Right Side: Roles & Responsibilities */}
               <div className="col-lg-8 d-flex justify-content-start align-items-center">
                 <div className="card-body text-light">
@@ -58,6 +52,9 @@ const Experience = () => {
                       ))}
                     </ul>
                   )}
+                  <div className="w-100 text-start ms-3">
+                    <a className="fs-5 text-primary" href="/experience-detail-view">More details</a>
+                  </div>
                 </div>
               </div>
             </div>
